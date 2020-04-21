61218c818eb1e52a535fd01b30ef10f30fb1c92d
manitu85 <manitu.psy@gmail.com>
2020-04-21 23:50:00 +0200

    complete clip mask text gradient at large hero

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp'
 import webpack from 'webpack'
 
-gulp.task('scripts', (callback) => {
+gulp.task('scripts', callback => {
   webpack(require('../../webpack.config.js'), (err, stats) => {
       if(err) {
         console.log(err.toString())
