19541a970e790c564621593d67c2b719650208ea
manitu85 <manitu.psy@gmail.com>
2021-04-07 18:56:14 +0200

    #

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,11 @@
-import $ from 'jquery'
+import $ from "jquery";
 
-import Modal from './moduls/Modal'
-import MobileMenu from './moduls/MobileMenu'
-import StickyHeader from './moduls/StickyHeader'
+import Modal from "./moduls/modal";
+import MobileMenu from "./moduls/MobileMenu";
+import StickyHeader from "./moduls/StickyHeader";
 // import NavLinks from './moduls/NavLinks'
-
 
 const modal = new Modal();
 const mobileMenu = new MobileMenu();
 const stickyHeader = new StickyHeader();
 // const navLinks = new NavLinks();
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,34 @@
-const webpack = require('webpack'),
-      path = require('path');
+const webpack = require("webpack"),
+  path = require("path");
 
- const config = {
-
+const config = {
   entry: {
-    app: './src/js/app.js',
-    vendor: './src/js/vendor.js'
+    app: "./src/js/app.js",
+    vendor: "./src/js/vendor.js",
   },
   output: {
-    path: path.resolve(__dirname, './app/scripts'),
-    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, "./app/scripts"),
+    filename: "[name]-bundle.js",
   },
-   mode: process.env.NODE_ENV || 'development',
-   devtool: 'source-map',
-   stats: { colors: true },
-   module: {
-     rules: [
-       {
-         test: /\.js$/,
-         exclude: /node_modules/,
-         use: {
-           loader: 'babel-loader',
-           options: {
-             presets: ['@babel/preset-env']
-           }
-         },
-       },
-     ],
-   }
-}
+  mode: process.env.NODE_ENV || "development",
+  devtool: "source-map",
+  stats: { colors: true },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
 
 module.exports = config;
 
-
-
+// https://stackoverflow.com/questions/55921442/how-to-fix-referenceerror-primordials-is-not-defined-in-node
