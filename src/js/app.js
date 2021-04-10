f0db6a14612f579296d006bdf26cb184b3be901d
manitu85 <manitu.psy@gmail.com>
2021-04-10 17:45:23 +0200

    #

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { register } from "register-service-worker";
 import Modal from "./moduls/modal";
 import MobileMenu from "./moduls/MobileMenu";
 import StickyHeader from "./moduls/StickyHeader";
@@ -13,4 +14,31 @@ siteLogos.forEach((logo) => {
   logo.addEventListener("click", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
+});
+
+register("/service-worker.js", {
+  registrationOptions: { scope: "./" },
+  ready(registration) {
+    console.log("Service worker is active.");
+  },
+  registered(registration) {
+    console.log("Service worker has been registered.");
+  },
+  cached(registration) {
+    console.log("Content has been cached for offline use.");
+  },
+  updatefound(registration) {
+    console.log("New content is downloading.");
+  },
+  updated(registration) {
+    console.log("New content is available; please refresh.");
+  },
+  offline() {
+    console.log(
+      "No internet connection found. App is running in offline mode."
+    );
+  },
+  error(error) {
+    console.error("Error during service worker registration:", error);
+  },
 });
